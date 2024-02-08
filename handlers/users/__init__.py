d90e2b65bbdca8058385942d7f3cfc116d3db704
dark <ikromjon01021995@gmail.com>
2024-02-08 17:09:42 +0500

    html va markdownv2 text formatlash

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -1,5 +1,6 @@
 from . import help
 from . import start
+from . import textformatting
 from . import book_handler
 from . import wiki
 from . import tarjimon
--- loader.py
+++ loader.py
@@ -4,5 +4,6 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from data import config
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
+# bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.MARKDOWN_V2)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
